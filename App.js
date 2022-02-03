44c43017008cc16de8e0f05a6f106b0f6d78ac71
ilmiazhar <iing25leo60@gmail.com>
2022-02-03 17:56:56 +0700

    Learns How Flexbox works

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
-import { Button, StyleSheet, TextInput, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
+    <View style={{ padding: 10, flexDirection: "row", }}>
+      <View
+        style={{
+          backgroundColor: "red",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>A</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "blue",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>B</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "green",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>C</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+{
+  /* <View style={{ padding: 50 }}>
       <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
-        <TextInput
+       <TextInput
           placeholder="Enter your goal..."
           style={{
             borderColor: "black",
@@ -16,8 +57,5 @@ export default function App() {
         <Button title="ADD" style={{ padding: 10 }}></Button>
       </View>
       <View></View>
-    </View>
-  );
+    </View> */
 }
-
-const styles = StyleSheet.create({});
